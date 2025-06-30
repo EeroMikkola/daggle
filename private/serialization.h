a1cca676993202f71416b89f72731343f81129fd
EeroMikkola <eerojuhanimikkola@gmail.com>
2025-06-30 19:51:12 +0300

    fix: implement missing input port variant

--- private/serialization.h
+++ private/serialization.h
@@ -4,20 +4,17 @@
 
 #include <daggle/daggle.h>
 
-// port_flags {
-//     input
-//     output
-//     param
-//     recycle
-//     has_data
-//     linked
-// }
-
 // data_entry {
 //     type_stoff: u64 (reuse typename strings)
 //     size: u64
 //     bytes: byte[size]
 // }
+
+//typedef struct data_entry_1_s {
+//uint64_t type_stoff;
+//	uint64_t size;
+//	unsigned char bytes[/*size*/];
+//} data_entry_1_t; // size = 2*u64+size
 
 // strings like: "first\0second\0third\0", access by Nth character
 
@@ -31,6 +28,8 @@
 // datas_len: u64
 // datas: byte[datas_len], (bytes encode data_entry[])
 
+// Use versioned structs for backwards compatibility.
+
 typedef struct node_entry_1_s {
 	uint64_t name_stoff;
 	uint64_t type_stoff;
@@ -38,9 +37,25 @@ typedef struct node_entry_1_s {
 	uint64_t first_port_ptidx;
 } node_entry_1_t;
 
+typedef enum port_variant_1_e {
+	INPUT,
+	OUTPUT,
+	PARAMETER,
+} port_variant_1_t;
+
+typedef enum input_variant_1_e {
+	IMMUTABLE_REFERENCE, 
+	IMMUTABLE_COPY,
+	MUTABLE_REFERENCE,
+	MUTABLE_COPY
+} input_variant_1_t;
+
 typedef struct port_entry_1_s {
 	uint64_t name_stoff;
-	uint64_t edge_ptidx; // Set if flags has linked, else 0
-	uint64_t data_dtoff; // Set if flags has has_data, else 0
-	uint8_t flags;
+	uint64_t edge_ptidx; // max if unset
+	uint64_t data_dtoff; // max if unset
+	port_variant_1_t port_variant;
+	union {
+		input_variant_1_t input;
+	} port_specific;
 } port_entry_1_t;
